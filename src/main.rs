377a367d3a72188fffe2d4d3555f99e3eb74a8a5
Wck-iipi <110763795+Wck-iipi@users.noreply.github.com>
2024-05-23 13:53:16 +0530

    Added 200 response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -8,8 +9,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
             }
             Err(e) => {
                 println!("error: {}", e);
