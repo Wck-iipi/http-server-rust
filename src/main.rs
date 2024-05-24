b7805c42e534f147cbb9a8b983cba9aff3e7719a
Wck-iipi <110763795+Wck-iipi@users.noreply.github.com>
2024-05-25 00:23:08 +0530

    add post

--- src/main.rs
+++ src/main.rs
@@ -82,21 +82,6 @@ fn main() {
 
                     if type_of_request == "POST" {
                         let content = lines.last().unwrap().as_bytes();
-                        // let mut current_file = OpenOptions::new()
-                        //     .create_new(true)
-                        //     .write(true)
-                        //     .append(true)
-                        //     .open(std::path::Path::new(&dirname))
-                        //     .expect("Cannot open file");
-                        if std::path::Path::exists(std::path::Path::new(&filepath)) {
-                            std::fs::write(&filepath, content).expect("Cannot write to file");
-                        } else {
-                            let mut current_file =
-                                std::fs::File::create(&filepath).expect("Cannot create file");
-                            current_file
-                                .write_all(content)
-                                .expect("Cannot write to file");
-                        }
                         let mut file = std::fs::File::create(dirname.to_owned() + filename)
                             .expect("file not created");
 
